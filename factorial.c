e5a1410bf10e7c41ed7f66d3607801c75025c76f
Joe Thomas <joe.p.thomas92@gmail.com>
2017-03-07 17:52:14 -0800

    add spaces to factorial function

--- factorial.c
+++ factorial.c
@@ -2,12 +2,14 @@
 
 int main() {
   int i, n;
-  unsigned long ans = 1;
+  unsigned long int ans = 1;
   printf("Enter a number: ");
   scanf("%d",&n);
+
   for (i = 1; i <= n; ++i) {
     ans = ans * i;
   }
+
   printf("The factorial of %d is %lu.\n", n, ans);
   return 0;
 }
